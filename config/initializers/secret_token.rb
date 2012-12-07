0a6f9dc3147c201adf35cc22dca06e520fcda367
Geoff Wright <geoff@weartolook.com>
2012-12-07 17:25:16 +0000

    first swing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nurserycms::Application.config.secret_token = '42cd50af01c3561cd998fcab555ba1b6f136eadb1f882f87dd52c358f11942ec9631d95c1c5b63948873ab01c45eefc5941365d75114dffaa230b597baa68a05'
